ea1371963afbbb6c313a1e85bd2f450f14985811
hajae <rntlqvnf@naver.com>
2021-01-27 02:51:17 +0900

    Chore: Update

--- app/src/main/java/com/jiangdg/usbcamera/utils/HttpAsynTackCallback.java
+++
@@ -1,8 +0,0 @@
-package com.jiangdg.usbcamera.utils;
-
-import org.json.JSONObject;
-
-public interface HttpAsynTackCallback {
-    void onSuccess(JSONObject result);
-    void onFailure(Exception e);
-}
